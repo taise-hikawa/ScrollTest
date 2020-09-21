505f2cf55b3ea82d0bc52712d0497df2c95ac4fc
taisei.hikawa <cmmkarubi@gmail.com>
2020-09-22 01:24:02 +0900

    0922

--- ScrollTest/ViewController.swift
+++ ScrollTest/ViewController.swift
@@ -9,9 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var contentView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//        contentView.translatesAutoresizingMaskIntoConstraints = false
+//        contentView.widthAnchor.constraint(equalTo: self.view.widthAnchor).isActive = true
+//        contentView.heightAnchor.constraint(equalTo: self.view.heightAnchor,multiplier: 2).isActive = true
+//        contentView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+//        contentView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+    }
+    override func viewDidLayoutSubviews() {
+
+//      scrollView.contentSize = contentView.frame.size
+      scrollView.flashScrollIndicators()
+
     }
 
 
